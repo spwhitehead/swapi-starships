44f5cc862fc7797ae9e2577c60cc338352e749e3
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-18 11:04:03 -0600

    Lays out dataclasses

--- starships.py
+++ starships.py
@@ -1,3 +1,27 @@
 import requests
+from dataclasses import dataclass
 
-requests.get("https://swapi.dev/api/starships/9/")
+
+@dataclass
+class Film:
+    title: str
+    episode_id: int
+
+
+@dataclass
+class Pilot:
+    name: str
+    films: list[Film]
+
+
+@dataclass
+class Starship:
+    name: str
+    model: str
+    cost_in_credits: int
+    pilots: list[Pilot]
+
+
+response = requests.get("https://swapi.dev/api/starships")
+response.json()
+print()
